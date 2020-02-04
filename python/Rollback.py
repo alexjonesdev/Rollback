6be99dcd7ef1e554dae01bfdf82f6ee27cbb4c21
Alex Jones <alexjonesdev@gmail.com>
2020-02-04 14:57:53 -0500

    Added some classes

--- python/Rollback.py
+++ python/Rollback.py
@@ -1,16 +1,36 @@
 #---==IMPORTS==---
+import random
 
 #---==CLASSES==---
 class Player:
+    """The base player class."""
     def __init__(self, number = 0):
         self.number = number
 
-class PlayerGenerator:
-    def __init__(self):
-        self.players = []
+class Bot(Player):
+    """A non-playable player with automated functionality."""
+    def get_input(self):
+        """Randomly generates an input"""
+        pass
 
-    def add_player(self, player = None):
-        if player is None:
-            self.players.append(Player(number = len(self.players) + 1))
-        else:
-            self.players.append(player)
+#---==MAIN LOGIC==---
+
+
+#-------------------------------------------------------
+# class PlayerGenerator:
+#     instance = None
+#     class __PlayerGenerator:
+#         def __init__(self, players = []):
+#             self.players = players
+
+#     def __init__(self, players):
+#         if not PlayerGenerator.instance:
+#             PlayerGenerator.instance = PlayerGenerator.__PlayerGenerator(players)
+#         else:
+#             PlayerGenerator.instance.players = players
+
+#     def add_player(self, player = None):
+#         if player is None:
+#             self.players.append(Player(number = len(self.players) + 1))
+#         else:
+#             self.players.append(player)
